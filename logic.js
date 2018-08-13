e2d5b637f39e396c0c4a7f47fe14474bbcdcf8af
Christian Chung <christianchung11@gmail.com>
2018-08-12 23:40:10 -0400

    new update

---
+++ logic.js
@@ -0,0 +1,25 @@
+<!-- <div id="main-container" class="container">
+<section class="main-section">
+    <h1>Contact</h1>
+
+    <form id="contact-form">
+        <ul>
+            <li>
+                <h5 style="color:black;">Name</h5>
+                <input type="text" id="name" name="name" placeholder="John Smith" required="required" >
+            </li>
+            <li>
+                <h5 style="color:black;">Email</h5>
+                <input type="email" id="email" name="email" placeholder="example@gmail.com" required="required">
+            </li>
+            <li>
+                <h5 style="color:black;">Message</h5>
+                <textarea id="message" name="message" required="required"></textarea>
+            </li>
+        </ul>
+        <input type="submit">
+    </form>
+
+</section>
+
+</div> -->
